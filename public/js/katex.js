cf2fc26bd438b9ae2295e1d075b9c2e1dd2ebe4b
Seth <n0bud@protonmail.com>
2023-02-02 21:42:56 -0500

    public

--- public/js/katex.js
+++ public/js/katex.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+    // provides an escape for dollar sign chars if KaTeX is not desired
+    let posts = document.getElementsByClassName("post");
+
+    for (let i = 0; i < posts.length; i++) {
+        posts[i].innerHTML = posts[i].innerHTML.replace(/\\\$/g, '<span>$</span>');
+    }
+
     renderMathInElement(document.body, {
         delimiters: [
             {left: "$$", right: "$$", display: true},
